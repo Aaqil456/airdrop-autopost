cd0e82ae08cbdaa85d8ce9c749eb87e3e62c72c5
Aaqil Ahamad <72489957+Aaqil456@users.noreply.github.com>
2025-05-08 15:01:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -109,10 +109,11 @@ def fetch_tweets_rapidapi(username, max_tweets=10):
 
 # === Translate ===
 def translate_text_gemini(text):
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
     headers = {"Content-Type": "application/json"}
     prompt = (
         f"Translate the following tweet into Malay (Bahasa Melayu) only. "
+        f"make it sound like a 3rd person and instructional "
         f"for the word (I) translate it into (kami) in malay "
         f"Do not include English. Return just the translated version:\n\n\"{text}\""
     )
